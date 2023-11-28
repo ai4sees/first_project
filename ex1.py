1e19d1b964fa10f470784b3a62b7683a40ee8642
baashai <106022699+baashai@users.noreply.github.com>
2023-11-28 12:12:54 +0530

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,69 +1,78 @@
+import numpy as np
+import cv2
 import tensorflow as tf
-from tensorflow.keras import layers
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D
 
-# Define the VAE model
-class VAE(tf.keras.Model):
-    def __init__(self, latent_dim):
-        super(VAE, self).__init__()
-        self.encoder = tf.keras.Sequential([
-            layers.InputLayer(input_shape=(28, 28, 1)),
-            layers.Conv2D(filters=32, kernel_size=(3, 3), activation='relu'),
-            layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu'),
-            layers.Flatten(),
-            layers.Dense(latent_dim * 2),
-        ])
-        self.decoder = tf.keras.Sequential([
-            layers.Dense(latent_dim * 2),
-            layers.Reshape((7, 7, 2)),
-            layers.Conv2DTranspose(filters=64, kernel_size=(3, 3), activation='relu'),
-            layers.Conv2DTranspose(filters=32, kernel_size=(3, 3), activation='relu'),
-            layers.Conv2DTranspose(filters=1, kernel_size=(3, 3), activation='sigmoid'),
-        ])
+# Synthetic Data Creation
+def create_circle_image(diameter, canvas_size=(64, 64)):
+    canvas = np.zeros(canvas_size, dtype=np.uint8)
+    center = (canvas_size[0] // 2, canvas_size[1] // 2)
+    cv2.circle(canvas, center, diameter // 2, (255, 255, 255), -1)
+    return canvas
 
-    def encode(self, x):
-        mean, log_var = tf.split(self.encoder(x), num_or_size_splits=2, axis=-1)
-        return mean, log_var
+def create_triangle_image(side_length, canvas_size=(64, 64)):
+    canvas = np.zeros(canvas_size, dtype=np.uint8)
+    pt1 = (canvas_size[0] // 2, canvas_size[1] // 2 - side_length // 2)
+    pt2 = (pt1[0] - side_length // 2, pt1[1] + side_length // 2)
+    pt3 = (pt1[0] + side_length // 2, pt1[1] + side_length // 2)
+    triangle_cnt = np.array([pt1, pt2, pt3])
+    cv2.drawContours(canvas, [triangle_cnt], 0, (255, 255, 255), -1)
+    return canvas
 
-    def decode(self, z):
-        return self.decoder(z)
+normal_images = [create_circle_image(20) for _ in range(100)]
+anomalous_images = [create_triangle_image(30) for _ in range(10)]
 
-    def reconstruct(self, x):
-        mean, log_var = self.encode(x)
-        z = self.reparameterize(mean, log_var)
-        return self.decode(z)
+normal_images = np.array(normal_images)
+anomalous_images = np.array(anomalous_images)
 
-    def reparameterize(self, mean, log_var):
-        eps = tf.random.normal(shape=mean.shape)
-        return mean + eps * tf.exp(0.5 * log_var)
+# Model Design
+def build_autoencoder(input_shape):
+    input_img = Input(shape=input_shape)
+    x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
+    x = MaxPooling2D((2, 2), padding='same')(x)
+    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+    x = MaxPooling2D((2, 2), padding='same')(x)
+    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+    x = UpSampling2D((2, 2))(x)
+    x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
+    x = UpSampling2D((2, 2))(x)
+    decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+    autoencoder = Model(input_img, decoded)
+    autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+    return autoencoder
 
-# Load MNIST data
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
-x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+autoencoder = build_autoencoder((64, 64, 1))
 
-# Select a small subset of anomalous images
-anomalous_images = x_train[y_train == 1][:100]
+# Data Preprocessing
+normal_images = normal_images.astype('float32') / 255.
+anomalous_images = anomalous_images.astype('float32') / 255.
+normal_images = np.reshape(normal_images, (len(normal_images), 64, 64, 1))
+anomalous_images = np.reshape(anomalous_images, (len(anomalous_images), 64, 64, 1))
 
-# Train the VAE model
-vae = VAE(latent_dim=32)
-vae.compile(optimizer='adam', loss='binary_crossentropy')
-vae.fit(x_train, x_train, epochs=10)
+# Model Training
+autoencoder.fit(normal_images, normal_images, epochs=50, batch_size=128, shuffle=True)
 
-# Define anomaly threshold
-threshold = 20
+# Anomaly Detection
+def compute_reconstruction_error(data, model):
+    reconstructions = model.predict(data)
+    reconstruction_errors = tf.keras.losses.mse(reconstructions, data)
+    return reconstruction_errors.numpy()
 
-# Detect anomalies in test data
-reconstructed_images = vae.reconstruct(x_test)
-anomaly_scores = tf.keras.backend.mean(tf.keras.backend.square(x_test - reconstructed_images), axis=(1, 2, 3))
-anomalous_predictions = tf.cast(anomaly_scores > threshold, tf.float32)
+normal_errors = compute_reconstruction_error(normal_images, autoencoder)
+anomaly_errors = compute_reconstruction_error(anomalous_images, autoencoder)
 
-# Evaluate model performance
-precision = tf.keras.metrics.precision(y_test, anomalous_predictions)
-recall = tf.keras.metrics.recall(y_test, anomalous_predictions)
-f1_score = tf.keras.metrics.f1_score(y_test, anomalous_predictions)
+threshold = np.mean(normal_errors) + 2 * np.std(normal_errors)
 
-print('Precision:', precision.result().numpy())
-print('Recall:', recall.result().numpy())
-print('F1-score:', f1_score.result().numpy())
+def classify_errors(errors, threshold):
+    return ["anomaly" if error > threshold else "normal" for error in errors]
 
+classified_normal = classify_errors(normal_errors, threshold)
+classified_anomalies = classify_errors(anomaly_errors, threshold)
 
+# Display Results
+print("Threshold: ", threshold)
+print("Normal Errors: ", normal_errors)
+print("Anomaly Errors: ", anomaly_errors)
+print("Classified Normal: ", classified_normal)
+print("Classified Anomalies: ", classified_anomalies)
